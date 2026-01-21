aae4ae7efc7b1f3af2ad544f598cdea68a83fd23
michealtateatsu <quwieorpt@gmail.com>
2026-01-21 11:02:39 +0900

    ChouHiroshi先生のJavaSilver講座受講。Javaプログラムを作成。System.out.println, class, interface, lamda式を学習。

---
+++ src/main/DEMO03_interface.java
@@ -0,0 +1,22 @@
+package main;
+
+public class DEMO03_interface {
+
+	
+	interface Greeting{
+		void print();
+	}
+	
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		// 3. interface
+		Greeting printer = new Greeting() {
+			@Override
+			public void print() {
+				System.out.println("Hello, World!");
+			}
+		};
+		printer.print();
+	}
+}
